b612052ea2ff3efa439428573323074c3919e559
kimagure44 <ivan.lara.gomez.1977@gmail.com>
2021-01-15 02:06:03 +0100

    - Changing model with 'class' to 'function'

--- src/models/dataUser.js
+++ src/models/dataUser.js
@@ -1,24 +1,22 @@
-export class DataUser {
-  constructor (dataUser) {
-    const {
-      addressCity = '',
-      addressCountry = '',
-      addressPostalcode = '',
-      addressStreet = '',
-      birthDate = '',
-      email = '',
-      firstname = '',
-      lastname = ''
-    } = dataUser
-    return {
-      addressCity,
-      addressCountry,
-      addressPostalcode,
-      addressStreet,
-      birthDate,
-      email,
-      firstname,
-      lastname
-    }
+export const DataUser = (dataUser) => {
+  const {
+    addressCity = '',
+    addressCountry = '',
+    addressPostalcode = '',
+    addressStreet = '',
+    birthDate = '',
+    email = '',
+    firstname = '',
+    lastname = ''
+  } = dataUser
+  return {
+    addressCity,
+    addressCountry,
+    addressPostalcode,
+    addressStreet,
+    birthDate,
+    email,
+    firstname,
+    lastname
   }
 }
